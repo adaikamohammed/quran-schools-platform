f7b2cc1eb0ac418d8160430e120ef80ea81e1d6b
Adaika Mohammed <118262120+adaikamohammed@users.noreply.github.com>
2026-04-24 19:16:06 +0100

    ada35

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/public/Navbar";
 import HeroSection from "@/components/public/HeroSection";
-import WaqfSection from "@/components/public/WaqfSection";
+
 import FeaturesSection from "@/components/public/FeaturesSection";
 import HowItWorksSection from "@/components/public/HowItWorksSection";
 import Footer from "@/components/public/Footer";
@@ -11,7 +11,7 @@ export default function HomePage() {
       <Navbar />
       <main>
         <HeroSection />
-        <WaqfSection />
+
         <FeaturesSection />
         <HowItWorksSection />
       </main>
